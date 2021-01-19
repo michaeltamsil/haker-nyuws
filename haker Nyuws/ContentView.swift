b5882622ce6d9b2b4abd5f593010c6ad4c1d6295
Michael Tamsil <michaeltamsil@gmail.com>
2021-01-19 10:53:10 +0700

    Initial Commit

---
+++ haker Nyuws/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  haker Nyuws
+//
+//  Created by michael tamsil on 19/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
